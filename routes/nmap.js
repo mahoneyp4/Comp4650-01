7f03bb25c4bf4f0b54be40bec01b4a30ab75af7a
mahoneyp4 <mahoneyp4@wit.edu>
2020-03-27 00:20:25 -0400

    Add files via upload
    Added nmap queries

--- routes/nmap.js
+++ routes/nmap.js
@@ -1,11 +1,10 @@
 var nmap = require('node-nmap');
 var fs = require('fs');
+var isIp = require('is-ip');
 var express = require('express');
 var router = express.Router();
 
-//nmap.nodenmap.nmapLocation = "nmap";
-//let quickscan = new nmap.QuickScan('127.0.0.1 google.com');
-
+nmap.nmapLocation = "nmap";
 //Functions
 /**
  *
@@ -26,17 +25,91 @@ var router = express.Router();
  *  -> Make stuff readable
  *
  */
-function updateConfig(ent){
+
+
+function perfromScan(ipAddr, subnet, scanType, customString){
+    var curScan = null;
+    if(subnet){
+        var range = ipAddr+"/"+subnet;
+    }else{
+        var range = ipAddr;
+    }
+    var fullNmap = ["-p 1-65535", "-sS", "-sU", "-T4", "-A", "-v"];
+    function actionFunction(data){
+            //console.log(data);
+            //document.write("Percentage complete" + curScan.percentComplete());
+            console.log("Percentage complete" + curScan.percentComplete());
+        }
+    switch(scanType) {
+        case "QuickScan":
+            curScan = new nmap.QueuedQuickScan(range, actionFunction);
+            break;
+        case "OSPortScan":
+            curScan = new nmap.QueuedOsAndPortScan(range, actionFunction);
+            break;
+        case "IntenseScan":
+            curScan = new nmap.QueuedNmapScan(range, fullNmap, actionFunction);
+            break;
+        case "CustomScan":
+            var params = customSyntax(customString);
+            curScan = new nmap.QueuedNmapScan(range, params, actionFunction);
+            break;
+        }curScan.on('complete', function(data){
+            console.log(data);
+            console.log("total scan time" + curScan.scanTime);
+            saveJson(curScan);
+        });
+
+        curScan.on('error', function(error){
+            console.log(error);
+        });
+
+    curScan.startRunScan();
+    return curScan;
+}
+
+function customSyntax(customString){
+    //break down string
+    //regex catch for any bad syntax queries
+    //place command parameters in var output
+
+    var output = null;
+
+    return output;
+}
+
+function formatOutput(input){
+//better look
+}
+
+function exportCSV(input){
+//finding better way
+}
+
+function saveJson(ent){
     let data = JSON.stringify(ent);
-    fs.writeFile('./config.json', data, (err) => {
+    fs.writeFile('./test.json', data, (err) => {
         if (err) throw err;
-        console.log('Config Updated');
+        console.log('Json Saved');
     });
 }
 
+/* POST from users requesting NMAP scans */
+router.post('/', function(req, res, next) {
+    var ipAddr = req.body.ipAddr;
+    var subnet = req.body.subnet;
+    var scan = req.body.scan;
+    var customParam = req.body.customParam;
+    var curScan = perfromScan(ipAddr, subnet, scan, customParam)
+    console.log(curScan);
+    console.log("--------------------------------------");
+    console.log(curScan.results());
+    res.render('nmap', { title: 'Home Page' });
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send(quickscan);
+    res.render('nmap', { title: 'Home Page' });
 });
 
 module.exports = router;
