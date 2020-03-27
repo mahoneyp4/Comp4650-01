677a0d95a183723fd165c0babc02983043684d7a
crosbyc2 <crosbyc2@wit.edu>
2020-03-26 20:26:14 -0400

    net-snmp

--- routes/snmp.js
+++ routes/snmp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-
+var snmp = require('net-snmp');
+var session = snmp.createSession("192.168.0.219");
+var oids = ["."];
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -14,7 +15,21 @@ router.post('/', function(req, res){
     //res.send(req.body);\
     ip = req.body.ip;
     subnet= req.body.subnet;
-    
+    session.get(oids,function(error, varbinds){
+        if (error){
+            console.log(error);
+        }else{
+            for (var i = 0; i < varbinds.length; i++)
+            if (snmp.isVarbindError (varbinds[i]))
+                console.error (snmp.varbindError (varbinds[i]))
+            else
+                console.log (varbinds[i].oid + " = " + varbinds[i].value);
+        }
+  
+        session.close ();
+
+    });
+
 
     data = {
         ip: req.body.ip,
