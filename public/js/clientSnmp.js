6a714ba7e980cb3b059f82642b007df5dd5f4bee
crosbyc2 <crosbyc2@wit.edu>
2020-04-06 19:19:04 -0400

    form validation

--- public/js/clientSnmp.js
+++ public/js/clientSnmp.js
@@ -1,5 +1,63 @@
 //todo: check if user has entered enough information to click on submit
 
+function validateForm(){
+    var isValid = false;
+    var validIp = false;
+    var hasOidOrCheck = false;
+
+    var ip = document.getElementById('ip').value;
+    validIp = validateIpAddress(ip);
+
+    //var hasOid = !('' ==document.getElementById('oid').value);
+    
+    var hasOid = validateOid(document.getElementById('oid').value);
+
+    var hasBoxCheck= validateCheckBox(document.getElementsByClassName('box'));
+    isValid= (validIp & (hasBoxCheck||hasOid));
+    //window.alert("oid: "+ hasOid+ " box: "+hasBoxCheck)
+   
+    if(!isValid){
+        window.alert("Please be sure to enter an IP address and check a box or enter an OID")
+    }
+    return isValid;
+    }
+   
+
+
+function validateCheckBox(boxes){
+    var oneHasCheck = false;
+    for  (i= 0; i<boxes.length;i++){
+        if(boxes[i].checked) oneHasCheck=true;
+    }
+    return oneHasCheck;
+}
+function validateOid(oid){
+    var oidPattern = /^([\.,\d]*$)/;
+    
+    oidRes = oid.search(oidPattern)==0;
+    if(oid==''){
+        oidRes=false;
+    }
+    //window.alert(oidRes)
+    if(!oidRes){
+        //window.alert('Please enter a valid oid');
+    }
+    return oidRes;
+    
+}
+
+function validateIpAddress(ip){
+    //This is a regex for a valid IP that follows 000.000.000.000
+    var ipPattern = /^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/;
+    ipRes = (ip.search(ipPattern)==0);
+    //window.alert(res)
+    if(!ipRes){
+       // window.alert('Please enter a valid IP address');
+    }
+    //window.alert(ipRes)
+    return ipRes;
+}
+
 window.onload = function(){
     //console.log('loaded')
 
@@ -20,10 +78,12 @@ window.onload = function(){
         })
     })
 
-    document.getElementById('submitButton').addEventListener('click',function(){
-        
-    });
-
+   var checkSubmit = document.getElementById('mainForm');
+   checkSubmit.addEventListener('submit', function(e){
+    
+    if(!validateForm())
+        e.preventDefault();
+   });
 
 
 };
--- routes/snmp.js
+++ routes/snmp.js
@@ -4,14 +4,18 @@ var snmp = require("net-snmp");
 router.get('/', function(req, res) {
 
     res.render('snmp');
-});
 
+});
+var memOids='1.3.6.1.4.1.2021.4.3.0,1.3.6.1.4.1.2021.4.4.0,1.3.6.1.4.1.2021.4.5.0,1.3.6.1.4.1.2021.4.6.0,1.3.6.1.4.1.2021.4.11.0,1.3.6.1.4.1.2021.4.13.0,1.3.6.1.4.1.2021.4.14.0,1.3.6.1.4.1.2021.4.15.0'
 
 router.post('/', function(req, res){
     if(req.body.default){
         console.log('Default true')
-        ip ='192.168.0.219';
-        oids = ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
+        //ip ='192.168.0.219';
+        //oids = ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
+        
+        ip=req.body.ip;
+        oids=req.body.ip;
         session = snmp.createSession(ip, "public");
         var snmpResponse ='';
         session.get(oids, function (error,varbinds){
@@ -49,9 +53,27 @@ router.post('/', function(req, res){
         oids=req.body.oid;
         console.log("oids before split");
         
-        if(req.body.host)
+        if(req.body.host){
             if(oids=='')oids='1.3.6.1.2.1.1.5.0'; else oids=oids.concat(',','1.3.6.1.2.1.1.5.0');
-            
+        }
+        if(req.body.disk){
+            if(oids=='')oids='1.3.6.1.2.1.1.5.0'; else oids=oids.concat(',','1.3.6.1.2.1.1.5.0');
+        }
+        if(req.body.memory){
+            if(oids=='')oids=memOids; else oids=oids.concat(',',memOids);
+
+        }
+        if(req.body.cpu){
+            if(oids=='')oids='1.3.6.1.2.1.1.5.0'; else oids=oids.concat(',','1.3.6.1.2.1.1.5.0');
+        }
+        if(req.body.network){
+            if(oids=='')oids='1.3.6.1.2.1.1.5.0'; else oids=oids.concat(',','1.3.6.1.2.1.1.5.0');
+        }
+        if(req.body.activeUser){
+            if(oids=='')oids='1.3.6.1.2.1.1.5.0'; else oids=oids.concat(',','1.3.6.1.2.1.1.5.0');
+        }
+        
+           
         //////////////////////////////////////////
         console.log(oids);
         oids=oids.split(',');
